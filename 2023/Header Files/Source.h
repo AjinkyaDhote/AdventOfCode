33a9f9d3c414f3aff1b32962bc3d225fce6626f8
Ajinkya Dhote <adhote@panicbuttongames.com>
2023-08-18 13:40:27 -0500

    Added new folder for 2023 challenge. Added Day 1 - Calorie Counting solution.

---
+++ 2023/Header Files/Source.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+namespace Advent
+{
+	class Solutions2023
+	{
+	public:
+		static int CalorieCounting();
+		static int RockPaperScissors();
+	};
+}
